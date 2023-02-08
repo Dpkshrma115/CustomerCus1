06b1aa0b20d19146ec77215597280688e16b2d4f
Deepak <Deepak@DESKTOP-1SEO4JR.home>
2023-02-08 13:49:10 -0500

    GitKey is added

--- src/test/java/org/gitpractise/Practise_Git.java
+++ src/test/java/org/gitpractise/Practise_Git.java
@@ -5,6 +5,9 @@ public class Practise_Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Only a practise file");
+String GitKey = "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";
 	}
+	
+//	git key is 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
 
 }
