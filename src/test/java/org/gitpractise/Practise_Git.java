a4e007473537ddaa24ab302c94c8330cb951308d
Deepak <Deepak@DESKTOP-1SEO4JR.home>
2023-02-08 12:21:38 -0500

    Committing practice file Feb-8-23

---
+++ src/test/java/org/gitpractise/Practise_Git.java
@@ -0,0 +1,10 @@
+package org.gitpractise;
+
+public class Practise_Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Only a practise file");
+	}
+
+}
